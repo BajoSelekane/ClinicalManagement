26a6695784602f2dc9f10fae77b8da693a290cb4
Excellent Selekane <bajoselekane@gmail.com>
2025-12-05 17:50:39 +0200

    Identity Migration and Updating ClinicalDB

--- ClinicalManagement.Infrastructure/Data/ClinicalContextDB.cs
+++ ClinicalManagement.Infrastructure/Data/ClinicalContextDB.cs
@@ -1,11 +1,14 @@
 ﻿using ClinicalManagement.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ClinicalManagement.Infrastructure.Data
 {
-    public class ClinicalContextDB : DbContext
+  public class ClinicalContextDB :IdentityDbContext<IdentityUser>
+    //public class ClinicalContextDB(DbContextOptions options) : DbContext(options)
     {
-        public ClinicalContextDB(DbContextOptions options) : base(options)
+        public ClinicalContextDB(DbContextOptions<ClinicalContextDB> options) : base(options) 
         {
         }
         public DbSet<Patient> Patients { get; set; }
@@ -14,45 +17,42 @@ namespace ClinicalManagement.Infrastructure.Data
         public DbSet<TimeSlot> Timeslots { get; set; }
         public DbSet<Clinic> Clinics { get; set; }
         public DbSet<LoginModal> LoginModals { get; set; }
-
+        public DbSet<RegisterModal> RegisterModals { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //seed Patients data
-            //modelBuilder.Entity<Patient>().HasData(
-            //     new Patient { Id = Guid.Parse("9e609d11-aea4-46b1-ba7e-a18fc5db34a1"), DateOfBirth = new DateTime(2000, 1, 15), Description = "Health", Email = "Thabo@yahoo.com", IDNumber = 8812055058081L, Name = "Thabo", PhoneNumber = "27684324014" },
-            //     new Patient { Id = Guid.Parse("8e609d11-bea4-46b1-ba7e-a18fc5db34a2"), DateOfBirth = new DateTime(1990, 07, 29), Description = "Consultation", Email = "Oupa@yahoo.com", IDNumber = 9602150570815L, Name = "Oupa", PhoneNumber = "27731219291" },
-            //     new Patient { Id = Guid.Parse("7e609d11-cea4-46b1-ba7e-a18fc5db34a3"), DateOfBirth = new DateTime(1998, 09, 12), Description = "General", Email = "Winnie@yahoo.com", IDNumber = 8508250570815L, Name = "Winnie", PhoneNumber = "27731218587" }
-
-            //    );
-
-            //seed Doctors data
-            //modelBuilder.Entity<Doctor>().HasData(
-            //   new Doctor { Id = Guid.Parse("1e609d11-bea4-46b1-ba7e-a18fc5db34aa"), Name = "DR Mathew ", Specialty = "General", Email = "Mathew@Medicare.com" },
-            //   new Doctor { Id = Guid.Parse("2e609d11-bea4-46b1-ba7e-a18fc5db34ab"), Name = "DR Selekon ", Specialty = "Gynecologist", Email = "Selekon@Medicare.com" },
-            //   new Doctor { Id = Guid.Parse("3e609d11-bea4-46b1-ba7e-a18fc5db34ac"), Name = "DR Zeelensky ", Specialty = "Heart-Surgeon", Email = "Zeelensky@Gov.com" }
-            //   );
-
-            //var clinicId = Guid.Parse("6e609d11-bea4-46b1-ba7e-a18fc5db34a6");
-            //modelBuilder.Entity<Clinic>().HasData(new Clinic { Id = clinicId, Name = "Central PHC", Address = "123 Main St" });
-            //// Create timeslots for next 3 days
-            //var slots = new List<TimeSlot>();
-            //for (int d = 0; d < 3; d++)
-            //{
-            //    var date = DateTime.UtcNow.Date.AddDays(d).AddHours(8);
-            //    for (int i = 0; i < 8; i++)
-            //    {
-            //        slots.Add(new TimeSlot { Id = Guid.Parse("5e609d11-bea4-46b1-ba7e-a18fc5db34az"), ClinicId = clinicId, StartTime = date.AddHours(i), EndTime = date.AddHours(i + 1), IsAvailable = false });
-            //    }
-            //}
-
-
+         
             base.OnModelCreating(modelBuilder);
         }
 
-        protected ClinicalContextDB()
-        {
-        }
+    
 
        
     }
 }
+//seed Patients data
+//modelBuilder.Entity<Patient>().HasData(
+//     new Patient { Id = Guid.Parse("9e609d11-aea4-46b1-ba7e-a18fc5db34a1"), DateOfBirth = new DateTime(2000, 1, 15), Description = "Health", Email = "Thabo@yahoo.com", IDNumber = 8812055058081L, Name = "Thabo", PhoneNumber = "27684324014" },
+//     new Patient { Id = Guid.Parse("8e609d11-bea4-46b1-ba7e-a18fc5db34a2"), DateOfBirth = new DateTime(1990, 07, 29), Description = "Consultation", Email = "Oupa@yahoo.com", IDNumber = 9602150570815L, Name = "Oupa", PhoneNumber = "27731219291" },
+//     new Patient { Id = Guid.Parse("7e609d11-cea4-46b1-ba7e-a18fc5db34a3"), DateOfBirth = new DateTime(1998, 09, 12), Description = "General", Email = "Winnie@yahoo.com", IDNumber = 8508250570815L, Name = "Winnie", PhoneNumber = "27731218587" }
+
+//    );
+
+//seed Doctors data
+//modelBuilder.Entity<Doctor>().HasData(
+//   new Doctor { Id = Guid.Parse("1e609d11-bea4-46b1-ba7e-a18fc5db34aa"), Name = "DR Mathew ", Specialty = "General", Email = "Mathew@Medicare.com" },
+//   new Doctor { Id = Guid.Parse("2e609d11-bea4-46b1-ba7e-a18fc5db34ab"), Name = "DR Selekon ", Specialty = "Gynecologist", Email = "Selekon@Medicare.com" },
+//   new Doctor { Id = Guid.Parse("3e609d11-bea4-46b1-ba7e-a18fc5db34ac"), Name = "DR Zeelensky ", Specialty = "Heart-Surgeon", Email = "Zeelensky@Gov.com" }
+//   );
+
+//var clinicId = Guid.Parse("6e609d11-bea4-46b1-ba7e-a18fc5db34a6");
+//modelBuilder.Entity<Clinic>().HasData(new Clinic { Id = clinicId, Name = "Central PHC", Address = "123 Main St" });
+//// Create timeslots for next 3 days
+//var slots = new List<TimeSlot>();
+//for (int d = 0; d < 3; d++)
+//{
+//    var date = DateTime.UtcNow.Date.AddDays(d).AddHours(8);
+//    for (int i = 0; i < 8; i++)
+//    {
+//        slots.Add(new TimeSlot { Id = Guid.Parse("5e609d11-bea4-46b1-ba7e-a18fc5db34az"), ClinicId = clinicId, StartTime = date.AddHours(i), EndTime = date.AddHours(i + 1), IsAvailable = false });
+//    }
+//}
--- ClinicalManagement.Infrastructure/Migrations/20251205154509_InitClinic.Designer.cs
+++ ClinicalManagement.Infrastructure/Migrations/20251205154509_InitClinic.Designer.cs
@@ -4,6 +4,7 @@ using ClinicalManagement.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClinicalManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(ClinicalContextDB))]
-    partial class ClinicalContextDBModelSnapshot : ModelSnapshot
+    [Migration("20251205154509_InitClinic")]
+    partial class InitClinic
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
